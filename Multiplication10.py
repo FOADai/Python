35955b527b5307e6c68dabc7ee98d5a33e1082bf
foad.aghayari <100021393+FOADai@users.noreply.github.com>
2022-02-21 22:36:47 +0330

    Add files via upload

---
+++ Multiplication10.py
@@ -0,0 +1,17 @@
+number_m = int ( input('Please enter a number= '))
+if number_m >= 1 and number_m < 10 :
+    print (10)
+elif number_m >= 10 and number_m < 20 :
+    print (20)
+elif number_m >= 20 and number_m < 30 :
+    print (30)
+elif number_m >= 30 and number_m < 40 :
+    print (40)
+elif number_m >= 40 and number_m < 50 :
+    print (50)
+elif number_m >= 50 and number_m < 60 :
+    print (60)
+elif number_m >= 60 and number_m < 70 :
+    print (70)
+elif number_m >= 70 and number_m < 80 :
+    print (80)
