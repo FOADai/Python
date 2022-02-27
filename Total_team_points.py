bd64312552886f938abe76800ecb00c52a3fb814
Foad Aghayari <100021393+FOADai@users.noreply.github.com>
2022-02-27 13:22:40 +0330

    Update Total_team_points.py

--- Total_team_points.py
+++ Total_team_points.py
@@ -1,4 +1,4 @@
-point = int(input())
+point = int(input('please number 1 or 3 or 0: '))
 index = 1
 succ= 0
 sume = 0
@@ -9,7 +9,7 @@ while index <= 30:
     sume = sume + point    
     index = index + 1
     if index <= 30:
-        point = int(input())
+        point = int(input('please enter number 1 or 3 or 0: '))
     
 print(sume,succ)
 
